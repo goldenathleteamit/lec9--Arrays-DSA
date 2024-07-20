4d007c2231580f4c3c48aa28c471e014296d12b7
goldenathleteamit <120368657+goldenathleteamit@users.noreply.github.com>
2024-07-20 19:00:51 +0530

    Update minmaxarray.cpp

--- minmaxarray.cpp
+++ minmaxarray.cpp
@@ -1,41 +1,30 @@
 #include<iostream>
 using namespace std;
 
-//int main(){
-//	int size;
-//	cout<<"size is";
-//	cin>>size;
-//	
-//	int arr[size];
-//	for(int i=0;i<size;i++){
-//		cin>>arr[i];
-//	};
-//	int max=arr[0];
-//	for(int i=0;i<size;i++){
-//		if(max<arr[i]){
-//			max=arr[i];
-//		}
-//	}
-//	cout<<max<<endl;
-//	
-//	int min=arr[0];
-//	for(int i=0;i<size;i++){
-//		if(min>arr[i]){
-//			min=arr[i];
-//		}
-//	}
-//	cout<<min;
-//}
-
-//linear search
 int main(){
-	int arr[]={2,3,4,5,7};
-	int key=4;
-	int ans=-1;
+	int size;
+	cout<<"size is";
+	cin>>size;
 	
-	for(int i=0;i<5;i++){
-		if(arr[i]==key){
-			ans=i;	
+	int arr[size];
+	for(int i=0;i<size;i++){
+		cin>>arr[i];
+	};
+	int max=arr[0];
+	for(int i=0;i<size;i++){
+		if(max<arr[i]){
+			max=arr[i];
 		}
-	}cout<<ans;
+	}
+	cout<<max<<endl;
+	
+	int min=arr[0];
+	for(int i=0;i<size;i++){
+		if(min>arr[i]){
+			min=arr[i];
+		}
+	}
+	cout<<min;
 }
+
+
